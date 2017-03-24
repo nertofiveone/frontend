f9f96a13a9e79c33584c4f5b35a922173b80eb9b
Alex Sanders <a@snd.rs>
2017-03-24 11:13:45 +0000

    fix lint errors with lib/add-event-listener.js after  transform to ES6

--- static/src/javascripts/lib/add-event-listener.js
+++ static/src/javascripts/lib/add-event-listener.js
@@ -1,24 +1,40 @@
+// @flow
 let supportsOptions = false;
 try {
-    const opts = Object.defineProperty({}, 'passive', {
-        get() {
-            supportsOptions = true;
-        }
-    });
+    const opts = Object.defineProperty(
+        {},
+        'passive',
+        ({
+            get() {
+                supportsOptions = true;
+            },
+        }: Object)
+    );
     window.addEventListener('test', null, opts);
-} catch (e) { /* noop */ }
+} catch (e) {
+    /* noop */
+}
 
-export default addEventListener;
-
-function addEventListener(node, eventName, eventHandler, options = {}) {
+function addEventListener(
+    node: Node,
+    eventName: string,
+    eventHandler: Function,
+    options: Object = {}
+) {
     if (supportsOptions) {
         node.addEventListener(eventName, eventHandler, options);
     } else if (options.once) {
-        node.addEventListener(eventName, function boundEventHandler(evt) {
-            eventHandler.call(this, evt);
-            node.removeEventListener(eventName, boundEventHandler);
-        }, options.capture);
+        node.addEventListener(
+            eventName,
+            function boundEventHandler(evt) {
+                eventHandler.call(this, evt);
+                node.removeEventListener(eventName, boundEventHandler);
+            },
+            options.capture
+        );
     } else {
         node.addEventListener(eventName, eventHandler, options.capture);
     }
 }
+
+export default addEventListener;
